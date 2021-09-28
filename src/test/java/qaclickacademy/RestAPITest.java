7bf6e1cc72aff39b5575e2865a59ac37dc8a22df
mahamud <mahmud.fahim1991@gmail.com>
2021-09-28 15:10:00 -0400

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	
+	@Test
+	public void PostJIRa()
+	{
+		System.out.println("PostJIRA");
+	}
+	@Test
+	public void delateTwitter()
+	{
+		System.out.println("delateTwitter");
+	}
+
+}
